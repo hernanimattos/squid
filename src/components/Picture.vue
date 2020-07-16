56a2d82ebab8d6769a36b5e712ebdfde16008d2a
Hernani Mattos <hernani.luiz@gmail.com>
2020-07-15 21:11:23 -0300

    Adjust of components

---
+++ src/components/Picture.vue
@@ -0,0 +1,39 @@
+<template>
+	<section class="wrapper-picture">
+		<picture
+			class="picture"
+			v-for="media in midias"
+			:key="media.metadados.idFacebook"
+		>
+			<img
+				src="mdn-logo-narrow.png"
+				alt="MDN"
+			>
+		</picture>
+	</section>
+</template>
+
+<script>
+export default {
+	name: 'Picture',
+	props: {
+		medias: {
+			type: Array,
+			required: true
+		}
+	}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.wrapper-picture {
+	display: grid;
+	grid-gap: 1rem;
+	grid-template-columns: auto auto auto auto;
+}
+
+.picture:hover {
+	background-color: red;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="home">
+		<section class="picture-wrapper">
+
+			<Picture />
+		</section>
+	</div>
+</template>
+
+<script>
+import Picture from '@/components/Picture.vue';
+
+export default {
+	name: 'Home',
+	components: {
+		Picture
+	}
+};
+</script>
