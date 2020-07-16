a293859e89bdde509700f19c02212916770d10ee
Hernani Mattos <hernani.luiz@gmail.com>
2020-07-16 12:05:21 -0300

    cadd components and import state of test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,38 @@
 <template>
-	<div class="home">
-		<section class="picture-wrapper">
-
-			<Picture />
+	<div class="container">
+		<Top />
+		<section>
+			<Picture :medias="medias" />
 		</section>
 	</div>
 </template>
 
 <script>
+import { createNamespacedHelpers } from 'vuex';
+const { mapState, mapActions } = createNamespacedHelpers('test');
 import Picture from '@/components/Picture.vue';
+import Top from '@/components/Top.vue';
 
 export default {
 	name: 'Home',
+	computed: {
+		...mapState(['medias']),
+	},
+	methods: {
+		...mapActions(['getMedias']),
+	},
+	created() {
+		this.getMedias();
+	},
 	components: {
-		Picture
-	}
+		Picture,
+		Top,
+	},
 };
 </script>
+<style>
+.container {
+	margin: 0 auto;
+	max-width: 1200px;
+}
+</style>
