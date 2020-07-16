97e4740f39a5ad6de786c3cb6b44ac056aba8333
Hernani Mattos <hernani.luiz@gmail.com>
2020-07-16 12:02:41 -0300

    Add icon and date filter

--- src/main.js
+++ src/main.js
@@ -1,12 +1,41 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+	faUserSecret,
+	faAt,
+	faHeart,
+	faComment,
+	faCalendarAlt,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-Vue.config.productionTip = false
+library.add(faUserSecret, faAt, faHeart, faComment, faCalendarAlt);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.filter('dateformat', function(value) {
+	const date = new Date(value);
+
+	const day = date.getDate();
+	const month = date.getMonth();
+	const year = date.getFullYear();
+
+	const hour = date.getHours();
+	const minutes =
+		date.getMinutes() <= 9 ? `0${date.getMinutes()}` : date.getMinutes();
+
+	if (!value) return '';
+	value = value.toString();
+	return `${day}/${month}/${year} ${hour}:${minutes}`;
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	render: (h) => h(App),
+}).$mount('#app');
