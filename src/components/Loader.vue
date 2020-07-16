793c0c0b6e1e35db5fc929d84ec1495becb493d5
Hernani Mattos <hernani.luiz@gmail.com>
2020-07-16 16:32:35 -0300

    Loader

---
+++ src/components/Loader.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="loader">
+		<img src="@/assets/loader.svg" alt="" />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Loader',
+};
+</script>
+
+<style scoped>
+loader {
+	position: fixed;
+	width: 100%;
+	height: 100%;
+	background-color: opacit();
+	top: 0;
+	left: 0;
+	z-index: 3;
+	bottom: 0;
+	background-color: rgba(241, 242, 243, 0.79);
+}
+
+.loader img {
+	position: absolute;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -11,30 +11,94 @@ export default new Vuex.Store({
     test: {
       namespaced: true,
       state: {
+        loader: false,
         nextUrl: '',
         medias: [],
-        count: ''
+        count: '',
+        qtd: 5
       },
       mutations: {
         getMedias(state, payload) {
-          state.media = payload;
-
+          state.medias = payload;
+        },
+        getNextUrl(state, payload) {
+          state.nextUrl = payload
+        },
+        setQtd(state, payload) {
+          state.qtd = payload
+        },
+        loaderStatus(state, payload) {
+          state.loader = payload
         }
       },
       actions: {
         async getMedias({
           commit
+        }, {
+          qtd
         }) {
+          commit('loaderStatus', true)
 
           try {
-            const medias = await HTTP.get('/test')
-            commit('getUsers', medias)
+            const {
+              data: {
+                medias,
+                pagination
+              }
+            } = await HTTP.get(`/test?count=${qtd}`)
+            const {
+              next_url
+            } = pagination;
+
+            commit('getMedias', medias)
+            commit('getNextUrl', next_url)
+            commit('setQtd', qtd)
+            commit('loaderStatus', false)
+
+            return Promise.resolve(medias)
+          } catch (error) {
+            return Promise.reject(error)
+          }
+        },
+        async getNextPage({
+          commit
+        }, {
+          url,
+          qtd
+        }) {
+          commit('loaderStatus', true)
+          try {
+            const {
+              data: {
+                medias,
+                pagination: {
+                  next_url
+                }
+              }
+            } = await HTTP.get(`/test?count=${qtd}`, {
+              next_url: url
+            })
+            commit('setQtd', qtd)
+            commit('getMedias', medias)
+            commit('getNextUrl', next_url)
+            commit('loaderStatus', false)
+
+
             return Promise.resolve(medias)
           } catch (error) {
             return Promise.reject(error)
           }
         }
       },
+      getters: {
+        parseNextUrl(state) {
+          return encodeURIComponent(state.nextUrl)
+        },
+        addQtd(state) {
+          let newQtd = state.qtd + 5;
+          return newQtd
+        }
+      }
     }
   }
 })
