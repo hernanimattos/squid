c4d2170dc7fed1c7bac6ecc1ae3eaa41d012a667
Hernani Mattos <hernani.luiz@gmail.com>
2020-07-16 16:36:21 -0300

    Loder, dependencies configs

--- src/main.js
+++ src/main.js
@@ -2,40 +2,51 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import { library } from '@fortawesome/fontawesome-svg-core';
 import {
-	faUserSecret,
-	faAt,
-	faHeart,
-	faComment,
-	faCalendarAlt,
+  library
+} from '@fortawesome/fontawesome-svg-core';
+import {
+  faUserSecret,
+  faAt,
+  faHeart,
+  faComment,
+  faCalendarAlt,
+  faArrowCircleRight
 } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import {
+  FontAwesomeIcon
+} from '@fortawesome/vue-fontawesome';
 
-library.add(faUserSecret, faAt, faHeart, faComment, faCalendarAlt);
+library.add(
+  faUserSecret,
+  faAt,
+  faHeart,
+  faComment,
+  faCalendarAlt,
+  faArrowCircleRight);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.filter('dateformat', function(value) {
-	const date = new Date(value);
+  const date = new Date(value);
 
-	const day = date.getDate();
-	const month = date.getMonth();
-	const year = date.getFullYear();
+  const day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
 
-	const hour = date.getHours();
-	const minutes =
-		date.getMinutes() <= 9 ? `0${date.getMinutes()}` : date.getMinutes();
+  const hour = date.getHours();
+  const minutes =
+    date.getMinutes() <= 9 ? `0${date.getMinutes()}` : date.getMinutes();
 
-	if (!value) return '';
-	value = value.toString();
-	return `${day}/${month}/${year} ${hour}:${minutes}`;
+  if (!value) return '';
+  value = value.toString();
+  return `${day}/${month}/${year} ${hour}:${minutes}`;
 });
 
 Vue.config.productionTip = false;
 
 new Vue({
-	router,
-	store,
-	render: (h) => h(App),
+  router,
+  store,
+  render: (h) => h(App),
 }).$mount('#app');
